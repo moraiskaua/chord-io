01ec8db33115d92c8d0786e8c30b0755fa53782f
moraiskaua <moraiskaua03@gmail.com>
2024-11-13 17:50:49 -0300

    fix: change fonts folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,13 @@ import localFont from 'next/font/local';
 import './globals.css';
 
 const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
+  src: './assets/fonts/GeistVF.woff',
   variable: '--font-geist-sans',
   weight: '100 900',
 });
 
 const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
+  src: './assets/fonts/GeistMonoVF.woff',
   variable: '--font-geist-mono',
   weight: '100 900',
 });
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='pt-BR'>
+    <html lang="pt-BR">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
