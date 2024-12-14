e701eee18d3b15d131110e7565604da55c89a052
moraiskaua <moraiskaua03@gmail.com>
2024-12-14 10:59:17 -0300

    chore: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className='h-screen'>
+      <section className='container mx-auto mt-10'>
+        <h1 className='text-center text-3xl font-bold'>Chord.io</h1>
+        <h3 className='text-center text-2xl font-bold'>Em contrução...🚧</h3>
+      </section>
+    </main>
+  );
+}
