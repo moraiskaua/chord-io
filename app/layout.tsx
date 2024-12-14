5f056f222bfb853384f5ae21e55ee8bb4e2f7ca0
moraiskaua <moraiskaua03@gmail.com>
2024-12-14 16:45:25 -0300

    feat: create session provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import { Geist, Geist_Mono } from 'next/font/google';
+import { SessionProvider } from 'next-auth/react';
 import { Toaster } from 'react-hot-toast';
 import type { Metadata } from 'next';
 import './globals.css';
+import { auth } from '@/lib/auth';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -18,18 +20,22 @@ export const metadata: Metadata = {
   description: 'Adivinhe o acorde do dia',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+
   return (
     <html lang="pt-BR">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Toaster />
-        {children}
+        <SessionProvider session={session}>
+          <Toaster />
+          {children}
+        </SessionProvider>
       </body>
     </html>
   );
--- app/play/welcome/layout.tsx
+++ app/play/welcome/layout.tsx
@@ -1,13 +1,10 @@
 import { ReactNode } from 'react';
-import { SessionProvider } from 'next-auth/react';
 
 const WelcomeLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <SessionProvider>
-      <main className="h-screen">
-        <section className="h-full container mx-auto">{children}</section>
-      </main>
-    </SessionProvider>
+    <main className="h-screen">
+      <section className="h-full container mx-auto">{children}</section>
+    </main>
   );
 };
 
